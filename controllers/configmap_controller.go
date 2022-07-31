163360ec07de7a94126b2c8ae9171b1f0124dd38
cheloghm <cheloghm@gmail.com>
2022-07-31 22:53:36 +0100

    updated commit

--- controllers/configmap_controller.go
+++ controllers/configmap_controller.go
@@ -1,29 +1,14 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
-
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
+	configfunc "ConfigFunctions.go"
 	configmapcontrollerv1 "cluster-config/weave/api/v1"
 )
 
@@ -32,6 +17,11 @@ type ConfigMapReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const (
+	configMapNamespace = "metadata.namespace"
+	kind = "ClusterConfigMap"
+)
 
 //+kubebuilder:rbac:groups=configmap-controller.cluster-config,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=configmap-controller.cluster-config,resources=configmaps/status,verbs=get;update;patch
@@ -47,50 +37,98 @@ type ConfigMapReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *ConfigMapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := r.Log.WithValues("ClusterConfigMap", req.NamespacedName)
 
-	println("Checking configMaps")
-	url, err := url.Parse('{}/api/v1/namespaces/{}/configmaps?watch=true"').format(baseURL, namespace)
+    /*
+        Step 0: Fetch the ConfigMap from the Kubernetes API.
+    */
+
+    var configMap corev1.ConfigMap
+    if err := r.Get(ctx, req.NamespacedName, &configMap); err != nil {
+		if apierrors.IsNotFound(err) {
+            // we'll ignore not-found errors, since we can get them on deleted requests.
+            return ctrl.Result{}, nil
+        }
+        log.Error(err, "unable to fetch configmap")
+        return ctrl.Result{}, err
+    }
+
+	/*
+        Step 1: Add or remove the configmap.
+    */
+
+    namespaceLabelsSelectores := app.kubernetes.io/managed-by
+	configNamespace := configMap.Spec.generateTo.namespaceSelectors.matchLabels.namespaceLabelsSelectores
+    kind := kind == "ClusterConfigMap"
+	labelShouldBePresent := configMap.Annotations[addPodNameLabelAnnotation] == "true"
+
+	//Supposed to get all available namespaces
+	var namespace[] := corev1.NamespaceAll(configMap)
+	var configMapsInANamespace[] := ConfigMapInterface.List(namespace)
+	if configMap.Kind == kind {
+        // This is the configs we want to check or work on
+		for _, s := range namespace {
+			if s.Namespace == configNamespace {
+				for _, s := range configMapsInANamespace{
+					if s.Name == configMap.Name
+						// The desired state and actual state of the Pod are the same.
+        				// No further action is required by the operator at this moment.
+        				log.Info("no update required")
+        				return ctrl.Result{}, nil
+					}
+				}else{
+					return ConfigMapInterface.Create(name, configNamespace)
+				}
+			}
+        		
+		}
 	
-    r := requests.get(url, stream=True)
-    // We issue the request to the API endpoint and keep the conenction open
-    for line in r.iter_lines():
-        obj := json.loads(line)
-        // We examine the type part of the object to see if it is MODIFIED
-        event_type := obj["type"]
-        // and we extract the configmap name because we'll need it later
-        configmap_name := obj["object"]["metadata"]["name"]
-        if event_type == "MODIFIED"{
-            println("Modification detected")
-            // If the type is MODIFIED then we extract the pod labels by
-			labels := Patch(configmap_name)
+		//Update configMaps
+		if err := r.Update(ctx, &configMap); err != nil {
+			if apierrors.IsConflict(err) {
+				// The ConfigMap has been updated since we read it.
+				// Requeue the ConfigMap to try to reconciliate again.
+				return ctrl.Result{Requeue: true}, nil
+			}
+			if apierrors.IsNotFound(err) {
+				// The ConfigMap has been deleted since we read it.
+				// Requeue the ConfigMap to try to reconciliate again.
+				ConfigMapInterface.Delete(configMap.Name, configMap.Namespace)
+				return ctrl.Result{Requeue: true}, nil
+			}
+			log.Error(err, "unable to update ConfigMap")
+			return ctrl.Result{}, err
 		}
 
-	if condition {
-		
-	}
-
-	return ctrl.Result{}, nil
+		return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ConfigMapReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&configmapcontrollerv1.ConfigMap{}).
+		For(&corev1.ConfigMap{}).
 		Complete(r)
 }
 
-		//My ConfigMap crud functions
+
+
+
+// ConfigMapsGetter has a method to return a ConfigMapInterface.
+// A group's client should implement this interface.
+type ConfigMapsGetter interface {
+	ConfigMaps(namespace string) ConfigMapInterface
+}
+
 // ConfigMapInterface has methods to work with ConfigMap resources.
 type ConfigMapInterface interface {
-	Create(*configmapcontrollerv1.ConfigMap) (*configmapcontrollerv1.ConfigMap, error)
-	Update(*configmapcontrollerv1.ConfigMap) (*configmapcontrollerv1.ConfigMap, error)
+	Create(*v1.ConfigMap) (*v1.ConfigMap, error)
+	Update(*v1.ConfigMap) (*v1.ConfigMap, error)
 	Delete(name string, options *api.DeleteOptions) error
 	DeleteCollection(options *api.DeleteOptions, listOptions api.ListOptions) error
-	Get(name string) (*configmapcontrollerv1.ConfigMap, error)
-	List(opts api.ListOptions) (*v1.ConfigMapList, error)
+	Get(name string) (*v1.ConfigMap, error)
+	List(namespace string, opts api.ListOptions) (*v1.ConfigMapList, error)
 	Watch(opts api.ListOptions) (watch.Interface, error)
-	Patch(name string, pt api.PatchType, data []byte, subresources ...string) (result *configmapcontrollerv1.ConfigMap, err error)
+	Patch(name string, pt api.PatchType, data []byte, subresources ...string) (result *v1.ConfigMap, err error)
 	ConfigMapExpansion
 }
 
@@ -109,11 +147,12 @@ func newConfigMaps(c *CoreClient, namespace string) *configMaps {
 }
 
 // Create takes the representation of a configMap and creates it.  Returns the server's representation of the configMap, and an error, if there is any.
-func (c *configMaps) Create(configMap *v1.ConfigMap) (result *v1.ConfigMap, err error) {
+func (c *configMaps) Create(name string, namespace string, configMap *v1.ConfigMap) (result *v1.ConfigMap, err error) {
 	result = &v1.ConfigMap{}
 	err = c.client.Post().
 		Namespace(c.ns).
 		Resource("configmaps").
+		Name(name).
 		Body(configMap).
 		Do().
 		Into(result)
@@ -134,100 +173,24 @@ func (c *configMaps) Update(configMap *v1.ConfigMap) (result *v1.ConfigMap, err 
 }
 
 // Delete takes name of the configMap and deletes it. Returns an error if one occurs.
-func (c *configMaps) Delete(name string, options *api.DeleteOptions) error {
+func (c *configMaps) Delete(name string, namespace string, options *api.DeleteOptions) error {
 	return c.client.Delete().
-		Namespace(c.ns).
+		Namespace(c.ns=namespace).
 		Resource("configmaps").
 		Name(name).
 		Body(options).
 		Do().
 		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *configMaps) DeleteCollection(options *api.DeleteOptions, listOptions api.ListOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("configmaps").
-		VersionedParams(&listOptions, api.ParameterCodec).
-		Body(options).
-		Do().
-		Error()
-}
-
-// Get takes name of the configMap, and returns the corresponding configMap object, and an error if there is any.
-func (c *configMaps) Get(name string) (result *v1.ConfigMap, err error) {
-	result = &v1.ConfigMap{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("configmaps").
-		Name(name).
-		Do().
-		Into(result)
-	return
 }
 
 // List takes label and field selectors, and returns the list of ConfigMaps that match those selectors.
-func (c *configMaps) List(opts api.ListOptions) (result *v1.ConfigMapList, err error) {
+func (c *configMaps) List(namespace string, opts api.ListOptions) (result *v1.ConfigMapList, err error) {
 	result = &v1.ConfigMapList{}
 	err = c.client.Get().
-		Namespace(c.ns).
+		Namespace(c.ns=namespace).
 		Resource("configmaps").
 		VersionedParams(&opts, api.ParameterCodec).
 		Do().
 		Into(result)
 	return
 }
-
-// Watch returns a watch.Interface that watches the requested configMaps.
-func (c *configMaps) Watch(opts api.ListOptions) (watch.Interface, error) {
-	return c.client.Get().
-		Prefix("watch").
-		Namespace(c.ns).
-		Resource("configmaps").
-		VersionedParams(&opts, api.ParameterCodec).
-		Watch()
-}
-
-// Patch applies the patch and returns the patched configMap.
-func (c *configMaps) Patch(name string, pt api.PatchType, data []byte, subresources ...string) (result *v1.ConfigMap, err error) {
-	result = &v1.ConfigMap{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("configmaps").
-		SubResource(subresources...).
-		Name(name).
-		Body(data).
-		Do().
-		Into(result)
-	return
-}
-
-def getConfigMapLabels(configmap):
-	println("Checking configMaps")
-	url, err := url.Parse('{}/api/v1/namespaces/{}/configmaps?watch=true"').format(baseURL, namespace)
-    r = requests.get(url)
-    // Issue the HTTP request to the appropriate endpoint
-    response = r.json()
-    // Extract the needed Selectors part from each object in the response
-    pod_labels_json = [i["spec"]["data"]
-              for i in response['items'] if i['spec']['configmap'] == "flaskapp-config"]
-    result = [list(l.keys())[0] + "=" + l[list(l.keys())[0]]
-              for l in pod_labels_json]
-    // The result is a list of labels
-    return result
-
-
-def getConfigMapLabels(configmap):
-	println("Checking configMaps")
-	url, err := url.Parse('{}/api/v1/namespaces/{}/configmaps?watch=true"').format(baseURL, namespace)
-    r = requests.get(url)
-    // Issue the HTTP request to the appropriate endpoint
-    response = r.json()
-    // Extract the the needed Selectors part from each object in the response
-    pod_labels_json = [i["spec"]["generateTo"]["namespaceSelectors"]
-              for i in response['items'] if i['spec']['configmap'] == "flaskapp-config"]
-    result = [list(l.keys())[0] + "=" + l[list(l.keys())[0]]
-              for l in pod_labels_json]
-    // The result is a list of labels
-    return result
